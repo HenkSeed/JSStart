0bb600395ba64c17c977d82f80563a65a24e218a
HenkSeed <55748514+HenkSeed@users.noreply.github.com>
2021-03-14 18:18:44 +0700

    cicles

--- script.js
+++ script.js
@@ -14,6 +14,28 @@ if (value === "PROMOCODE") {
 }
 */
 
+/*
 const a = 5;
 const value = a > 10 ? "Больше 10" : "Меньше или равно 10";
 console.log(value);
+*/
+
+/* 
+for (let i = 0; i < 5; i++) {
+	console.log(i);
+}
+*/
+
+/*
+let i = 0;
+while (i < 5) {
+	i++;
+	console.log(i);
+}
+*/
+
+let i = 5;
+do {
+	i++;
+	console.log(i);
+} while (i > 5 && i < 7);
