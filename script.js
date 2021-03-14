69327361105062060bf4e8c926d9db9847a80a7c
HenkSeed <55748514+HenkSeed@users.noreply.github.com>
2021-03-14 23:15:15 +0700

    functions

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
 /*
-const rain = false;
-if (rain) {
-	console.log("Take umbrella");
-} else {
-	console.log("Take water");
+function printConsole() {
+	console.log("Сообщение из функции");
 }
 
-const value = "PROMOCODE";
-if (value === "PROMOCODE") {
-	console.log("Discount");
-} else {
-	console.log("No Discount");
+printConsole();
+*/
+
+// multi-args
+/* Если несколько аргументов
+function printConsole(arg1, arg2, arg3) {
+	console.log('Сообщение из функции');
+	console.log(arg1 + 3); // (8)
+	console.log(arg2 + 7); // (12)
+	console.log(arg3 - 4); // (5)
 }
+
+printConsole(5, 5, 9);
 */
 
 /*
-const a = 5;
-const value = a > 10 ? "Больше 10" : "Меньше или равно 10";
-console.log(value);
-*/
-
-/* 
-for (let i = 0; i < 5; i++) {
-	console.log(i);
-}
+const foo = function (arr) {
+	console.log(arr);
+};
+foo([1, 2, 3]);
 */
 
 /*
-let i = 0;
-while (i < 5) {
-	i++;
-	console.log(i);
-}
+const bar = (args) => {
+	console.log(args);
+};
+bar('Привет!');
 */
 
-let i = 5;
-do {
-	i++;
-	console.log(i);
-} while (i > 5 && i < 7);
+const fun = function (arr) {
+	return arr.shift(); // Возвращает первый элемент массива
+};
+console.log(fun([1, 2, 3]));
+
+const bar = (args) => {
+	return 'Привет, ' + args;
+};
+console.log(bar('Макс!')); // Возвращает в консоль "Привет, Макс!"
