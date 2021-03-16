ceb34cb3bf159ee4396196578bc74f22b309851a
HenkSeed <55748514+HenkSeed@users.noreply.github.com>
2021-03-17 00:46:45 +0700

    dom

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
 /*
-function printConsole() {
-	console.log("Сообщение из функции");
-}
-
-printConsole();
-*/
-
-// multi-args
-/* Если несколько аргументов
-function printConsole(arg1, arg2, arg3) {
-	console.log('Сообщение из функции');
-	console.log(arg1 + 3); // (8)
-	console.log(arg2 + 7); // (12)
-	console.log(arg3 - 4); // (5)
-}
-
-printConsole(5, 5, 9);
-*/
-
-/*
-const foo = function (arr) {
-	console.log(arr);
-};
-foo([1, 2, 3]);
-*/
-
-/*
-const bar = (args) => {
-	console.log(args);
-};
-bar('Привет!');
-*/
-
 const fun = function (arr) {
 	return arr.shift(); // Возвращает первый элемент массива
 };
@@ -41,3 +8,29 @@ const bar = (args) => {
 	return 'Привет, ' + args;
 };
 console.log(bar('Макс!')); // Возвращает в консоль "Привет, Макс!"
+*/
+
+// console.log(document);
+// const head = document.getElementsByTagName('h1');
+/* 
+const head = document.getElementsByClassName('head');
+console.log(head[0]); 
+*/
+
+/*
+const head_01 = document.getElementById('main-head');
+console.log(head_01);
+*/
+
+/*
+const head1 = document.querySelector('#main-head');
+const head2 = document.querySelector('.head');
+const head3 = document.querySelector('h1');
+
+console.log(head1);
+console.log(head1);
+console.log(head1);
+*/
+
+const head = document.querySelector('[title="Javascript"]');
+console.log(head);
